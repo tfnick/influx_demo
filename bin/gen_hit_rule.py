3f12198f35e5b2128cc662e70614b2fb73a0065e
wuyuqing <wuyuqing@gr-data.com>
2019-06-10 09:53:33 +0800

    修改seq_num为field

--- bin/gen_hit_rule.py
+++ bin/gen_hit_rule.py
@@ -14,16 +14,16 @@ def emit(time_list):
 
     for item in time_list:
         
-        tmp_line = Template("m_rule_hit,cid=${cid},prod_code=${prod_code},etype=${etype},seq_num=${seq_num} rule_set=\"${rule_set}\",rule=\"${rule}\",hit=${hit} ${etime}")
+        tmp_line = Template("m_rule_hit,cid=${cid},prod_code=${prod_code},etype=${etype},rule_set=${rule_set},rule=${rule} seq_num=\"${seq_num}\",hit=${hit} ${etime}")
         
         line = tmp_line.substitute(
             cid = 1,
             prod_code='p1',
             etype = HelpUtil.random_choose_one(_etype),
-            seq_num = uuid.uuid1(),
-
             rule_set = HelpUtil.random_choose_one(_rule_set),
             rule = HelpUtil.random_choose_one(_rule),
+
+            seq_num = uuid.uuid1(),
             hit = 1,
 
             etime=item
@@ -35,9 +35,9 @@ if __name__ == '__main__':
     #时间区间
     year = 2019
     month = 6
-    day = 8
+    day = 10
     start_hour = 0
-    end_hour = 15 #max = 23
+    end_hour = 23 #max = 23
     #模拟多少条数据
     num_emit = 1000
 
